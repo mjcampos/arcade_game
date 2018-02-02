d0dc3722d10611cfcb7cda921bc9294cebec884f
Michael Campos <camposmj89@gmail.com>
2018-02-01 20:38:57 -0800

    Initial setup for project

--- js/app.js
+++ js/app.js
@@ -24,12 +24,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(dt) {
+    
+}
+
+Player.prototype.render = function() {
+
+}
+
+Player.prototype.handleInput = function(key) {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
